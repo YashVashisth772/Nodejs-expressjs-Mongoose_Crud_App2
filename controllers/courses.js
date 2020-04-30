ee90e722ab54c44133e9b185b465f16b5631e719
Vashisth <yash.vashisth@accenture.com>
2020-04-30 11:52:35 +0530

    add all files

---
+++ controllers/courses.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const CourseModel = mongoose.model("Course")
+
+router.get("/add", (req,res)=>{
+    res.render("add-course")
+});
+
+router.post("/add", (req,res)=>{
+
+    var course = new CourseModel();
+    course.courseName = req.body.courseName;
+    course.courseDuration = req.body.courseDuration;
+    course.courseFees = req.body.courseFees;
+    course.courseId = Math.ceil(Math.random() * 10000000);
+
+    course.save((err,doc)=> {
+        if(!err)
+        {
+            res.redirect("/course/list")
+        }
+        else{
+            res.send("Error Occured");
+        }
+    });
+    
+});
+
+router.get('/list', (req,res)=>{
+    CourseModel.find((err,docs) => {
+        if(!err)
+        {
+            res.render("list" , {data : docs})
+        }
+        else{
+            res.send("Error")
+        }
+    });
+});
+
+module.exports = router;
